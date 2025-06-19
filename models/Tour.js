4b0321f799bcda11766fc3cae75d1b04643758f1
ishan1408 <ishan@startupflora.com>
2025-06-19 17:38:28 +0530

    done

--- models/Tour.js
+++ models/Tour.js
@@ -1,45 +1,58 @@
 const mongoose = require("mongoose");
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A tour must have a name"],
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "A tour must have a name"],
+    },
+    duration: Number,
+    maxGroupSize: Number,
+    difficulty: {
+      type: String,
+      enum: ["easy", "medium", "difficult"],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, "A tour must have a price"],
+    },
+    summary: String,
+    description: String,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+    startDates: [Date],
+    startLocation: {
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+    },
   },
-  duration: Number,
-  maxGroupSize: Number,
-  difficulty: {
-    type: String,
-    enum: ["easy", "medium", "difficult"],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, "A tour must have a price"],
-  },
-  summary: String,
-  description: String,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  secretTour: {
-    type: Boolean,
-    default: false,
-  },
-  startDates: [Date],
-
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 // 🔐 Query Middleware - find hook
 tourSchema.pre(/^find/, function (next) {
-  console.log("Query Middleware is running")
+  console.log("Query Middleware is running");
   this.find({ secretTour: { $ne: true } });
   this.startTime = Date.now();
   next();
@@ -50,8 +63,8 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
-//Aggregation Middleware
-tourSchema.pre('aggregate', function (next) {
+// 📊 Aggregation Middleware
+tourSchema.pre("aggregate", function (next) {
   console.log("Aggregation Middleware is running...");
   next();
 });
